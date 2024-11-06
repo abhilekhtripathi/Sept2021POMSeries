20505034d4c297d79b4a8af8029b0649dabcd857
Abhilekh <abhilekh@saffire.com>
2024-11-06 18:09:28 +0530

    initial setup

---
+++ src/test/java/com/qa/opencart/test/BaseTest.java
@@ -0,0 +1,19 @@
+package com.qa.opencart.test;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class BaseTest {
+
+	@BeforeTest
+	public void setup() {
+
+	}
+
+	@AfterTest
+
+	public void tearDown() {
+
+	}
+
+}
